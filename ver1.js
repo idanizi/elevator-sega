9b696f4d25d75ed8c82e8f29f46f89e9021c6614
Idan Izicovich <idanizi@Idans-MacBook-Pro.local>
2020-02-04 02:33:58 +0200

    challenge 12

--- ver1.js
+++ ver1.js
@@ -1,48 +1,80 @@
 module.exports = {
     init: function (elevators, floors) {
 
-        let orders = new Set()
-        const sep = "."
+        let orders = [];
 
-        const sendElevatorToFloorIfNotOtherElevatorGoes = (elevator, floor, direction) => {
-            if (!orders.has(floor.floorNum() + sep + "up") && !orders.has(floor.floorNum() + sep + "down")) {
-                orders.add(floor.floorNum() + sep + direction)
-                elevator.goToFloor(floor.floorNum())
-            }
-        }
+        floors.forEach(floor => {
+            floor.on("up_button_pressed", () => orders.push({ floorNum: floor.floorNum(), direction: "up" }))
+            floor.on("down_button_pressed", () => orders.push({ floorNum: floor.floorNum(), direction: "down" }))
+        })
 
         for (let elevator of elevators) {
 
-            floors.forEach(floor => {
-                floor.on("up_button_pressed", () => sendElevatorToFloorIfNotOtherElevatorGoes(elevator, floor, "up"))
-                floor.on("down_button_pressed", () => sendElevatorToFloorIfNotOtherElevatorGoes(elevator, floor, "down"))
-            })
+            let timer;
+            let pressed = []
+            let currentOrder;
 
             elevator.on("idle", () => {
-                //if(orders.size < 1) return;
-                //let [floorNum, direction] = [...orders].pop().split(sep)
-                //elevator.goToFloor(floorNum)
+                // elevator.goingDownIndicator(true)
+                // elevator.goingUpIndicator(true)
+                timer = setInterval(() => {
+                    let floorNum;
+                    if (pressed.length > 0) {
+                        floorNum = pressed.shift().floorNum;
+                    }
+                    else if (orders.length > 0) {
+                        currentOrder = orders.shift()
+                        floorNum = currentOrder.floorNum;
+                    }
+                    else { return; }
+
+                    elevator.goToFloor(floorNum)
+                    const direction = elevator.currentFloor() < floorNum ? "up" : "down";
+                    // elevator.goingDownIndicator(direction === "down")
+                    // elevator.goingUpIndicator(direction === "up")
+                    if (timer) clearInterval(timer)
+                }, 1)
             })
 
             elevator.on("floor_button_pressed", floorNum => {
-                const direction = elevator.destinationQueue[elevator.destinationQueue - 1] > floorNum ? "down" : "up";
-                orders.add(floorNum + sep + direction)
-                elevator.goToFloor(floorNum)
+                //const next = elevator.destinationQueue[elevator.destinationQueue - 1];
+                const direction = elevator.currentFloor() > floorNum ? "down" : "up";
+                pressed.push({ floorNum, direction })
+                // elevator.goToFloor(floorNum)
             })
 
             elevator.on("stopped_at_floor", floorNum => {
-                const nextFloorNum = elevator.destinationQueue[0] || 0
-                if (nextFloorNum < floorNum)
-                    orders.delete(floorNum + sep + "down")
-                else
-                    orders.delete(floorNum + sep + "up")
+                pressed = pressed.filter(x => x.floorNum !== floorNum)
+                if (currentOrder != null) {
+                    const { direction } = currentOrder;
+                    elevator.goingDownIndicator(direction === "down")
+                    elevator.goingUpIndicator(direction === "up")
+                    currentOrder = null;
+                }
+
+
+                // const nextFloorNum = elevator.destinationQueue[0] || 0
+                // const direction = nextFloorNum > floorNum ? "up" : "down";
+                // elevator.goingDownIndicator(direction === "down")
+                // elevator.goingUpIndicator(direction === "up")
+                elevator.goingDownIndicator(elevator.currentFloor() === floors.length - 1)
+                elevator.goingUpIndicator(elevator.currentFloor() === 0)
             })
 
             elevator.on("passing_floor", (floorNum, direction) => {
-                if (orders.has(floorNum + sep + direction)) {
-                    elevator.goToFloor(floorNum, true)
+                let index;
+                index = pressed.findIndex(order => order.floorNum === floorNum)
+                if (index > -1) {
+                    elevator.goToFloor(pressed[index].floorNum, true)
+                    pressed.splice(index, 1)
+                    return;
                 }
 
+                index = orders.findIndex(order => order.floorNum === floorNum && order.direction === direction)
+                if (index > -1) {
+                    elevator.goToFloor(orders[index].floorNum, true)
+                    orders.splice(index, 1)
+                }
             })
         }
     },
